28f904828750e36a6c61abe034aced6fec07e69c
Agustin2001 <47432513+Agustin2001@users.noreply.github.com>
2019-02-07 21:55:23 -0300

    Primer commit
    Ejercicio 1 2 3 y 4

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombre = prompt("Ingrese su nombre y apellido", "Agustin");
+	alert("Hola, su nombre es " + nombre)
+}
+
