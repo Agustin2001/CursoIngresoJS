796bfe1f17b2b5efcee14fa884fd64cea67cc141
alumno <alumno>
2019-02-13 20:25:37 -0300

    (Parcial123)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
+    var precio;
+    var porcentaje;
 
+    precio=prompt("Precio: ");
+    porcentaje=prompt("Porcentaje de descuento");
+    
+   document.getElementById("elPrecioFinal").value = precio - (precio*porcentaje/100);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
 
+    ancho=prompt("Ancho del rectangulo");
+    largo=prompt("Largo del rectangulo");
+
+    ancho=parseInt('ancho');
+    ancho=parseInt('largo');
+
+    perimetro=(largo + largo + ancho + ancho);
+
+    alert("El perimetro es: "+perimetro);
 }
